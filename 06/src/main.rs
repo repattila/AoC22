8b9f2c7fa30ea83c7910d1c6c258050303f2d02f
repattila <repattila@protonmail.com>
2025-11-23 15:57:21 +0100

    06 part 2

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -1,17 +1,17 @@
 use aoc22_shared::*;
 
-fn insert_next_char(window: &mut[char; 4], next_char: char) {
-    for i in 0..3 {
+fn insert_next_char(window: &mut[char; 14], next_char: char) {
+    for i in 0..13 {
         window[i] = window[i+1]
     }
-    window[3] = next_char;
+    window[13] = next_char;
 }
 
 fn main() {
     if let Ok(lines) = read_lines("res/input.txt") {
         let line = lines.map_while(Result::ok).last().unwrap_or_default();
 
-        let mut window: [char; 4] = [' '; 4];
+        let mut window: [char; 14] = [' '; 14];
         let mut pos: usize = 0;
 
         for ch in line.chars() {
@@ -20,8 +20,8 @@ fn main() {
             pos += 1;
             
             let mut has_repeating: bool = false;
-            for i in 0..3 {
-                for j in i+1..4 {
+            for i in 0..13 {
+                for j in i+1..14 {
                     if window[i] == window[j] {
                         has_repeating = true;
                         break;
@@ -29,7 +29,7 @@ fn main() {
                 }
             }
 
-            if pos > 3 && !has_repeating {
+            if pos > 13 && !has_repeating {
                 println!("Marker pos: {pos}");
                 break;
             }
