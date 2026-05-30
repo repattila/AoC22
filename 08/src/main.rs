e276a851925cde84f8e0ec5c5908fa113062d4ae
repattila <repattila@protonmail.com>
2026-05-30 21:17:23 +0200

    08 part 1

--- 08/src/main.rs
+++ 08/src/main.rs
@@ -3,7 +3,7 @@ use aoc22_shared::*;
 fn main() {
     let mut trees: Vec<Vec<(bool, u32)>> = Vec::new();
 
-    if let Ok(lines) = read_lines("res/example.txt") {
+    if let Ok(lines) = read_lines("res/input.txt") {
         for line in lines.map_while(Result::ok) {
             let mut tree_line: Vec<(bool, u32)> = Vec::new();
 
@@ -20,10 +20,12 @@ fn main() {
 
     // from top to bottom
     for col in 0..trees[0].len() {
-        let mut prev_height: Option<u32> = None;
+        let mut max_height: Option<u32> = None;
 
         for row in 0..trees.len() {
-            if !update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut prev_height) {
+            update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut max_height);
+
+            if max_height == Some(9) {
                 break;
             }
         }
@@ -31,10 +33,12 @@ fn main() {
 
     // from bottom to top
     for col in 0..trees[0].len() {
-        let mut prev_height: Option<u32> = None;
+        let mut max_height: Option<u32> = None;
 
         for row in (1..trees.len()).rev() {
-            if !update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut prev_height) {
+            update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut max_height);
+            
+            if max_height == Some(9) {
                 break;
             }
         }
@@ -42,21 +46,25 @@ fn main() {
 
     // from left to right
     for row in 0..trees.len() {
-        let mut prev_height: Option<u32> = None;
+        let mut max_height: Option<u32> = None;
 
         for col in 0..trees[row].len() {
-            if !update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut prev_height) {
+            update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut max_height);
+
+            if max_height == Some(9) {
                 break;
-            }
+            }    
         }
     }
 
     // from left to right
     for row in 0..trees.len() {
-        let mut prev_height: Option<u32> = None;
+        let mut max_height: Option<u32> = None;
 
         for col in (1..trees[row].len()).rev() {
-            if !update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut prev_height) {
+            update_tree(trees.get_mut(row).unwrap().get_mut(col).unwrap(), &mut max_height);
+            
+            if max_height == Some(9) {
                 break;
             }
         }
@@ -78,20 +86,14 @@ fn main() {
     println!("{visible_count} trees are visible.")
 }
 
-fn update_tree(tree: &mut (bool, u32), prev_height: &mut Option<u32>) -> bool {
-    if let Some(ph) = prev_height {
-        if tree.1 > *ph {
+fn update_tree(tree: &mut (bool, u32), max_height: &mut Option<u32>) {
+    if let Some(mh) = max_height {
+        if tree.1 > *mh {
             tree.0 = true;
-            *prev_height = Some(tree.1);
-
-            return true;
-        } else {
-            return false;
+            *max_height = Some(tree.1);
         }
     } else {
         tree.0 = true;
-        *prev_height = Some(tree.1);
-
-        return true;
+        *max_height = Some(tree.1);
     }
 }
