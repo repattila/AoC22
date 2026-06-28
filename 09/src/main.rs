0c5d54046ec66c3349515ac7682d81b29c4468f1
repattila <repattila@protonmail.com>
2026-06-28 11:47:18 +0200

    09 part 2

--- 09/src/main.rs
+++ 09/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 fn main() {
     if let Ok(lines) = read_lines("res/input.txt") {
         let mut h_pos = (0, 0);
-        let mut t_pos = (0, 0);
+        let mut knots_pos = [(0, 0); 9];
         let mut t_pos_set: HashSet<(i32, i32)> = HashSet::new();
 
         for line in lines.map_while(Result::ok) {
@@ -35,51 +35,73 @@ fn main() {
                 h_pos.0 += s.0;
                 h_pos.1 += s.1;
 
-                let x_diff = h_pos.0 - t_pos.0;
-                let y_diff = h_pos.1 - t_pos.1;
+                let mut prev_knot_pos = h_pos;
+                for i in 0..9 {
+                    let mut curr_knot_pos = knots_pos[i];
 
-                if x_diff == 2 && y_diff == 0 {
-                    t_pos.0 += 1;
-                } else if x_diff == -2 && y_diff == 0 {
-                    t_pos.0 -= 1;
-                } else if y_diff == 2 && x_diff == 0 {
-                    t_pos.1 += 1;
-                } else if y_diff == -2 && x_diff == 0 {
-                    t_pos.1 -= 1;
-                } else if x_diff == 2 && y_diff == 1 {
-                    t_pos.0 += 1;
-                    t_pos.1 += 1;
-                } else if x_diff == -2 && y_diff == 1 {
-                    t_pos.0 -= 1;
-                    t_pos.1 += 1;
-                } else if x_diff == 2 && y_diff == -1 {
-                    t_pos.0 += 1;
-                    t_pos.1 -= 1;
-                } else if x_diff == -2 && y_diff == -1 {
-                    t_pos.0 -= 1;
-                    t_pos.1 -= 1;
-                } else if x_diff == 1 && y_diff == 2 {
-                    t_pos.0 += 1;
-                    t_pos.1 += 1;
-                } else if x_diff == 1 && y_diff ==-2 {
-                    t_pos.0 += 1;
-                    t_pos.1 -= 1;
-                } else if x_diff == -1 && y_diff == 2 {
-                    t_pos.0 -= 1;
-                    t_pos.1 += 1;
-                } else if x_diff == -1 && y_diff ==-2 {
-                    t_pos.0 -= 1;
-                    t_pos.1 -= 1;
-                } else if (x_diff < 2 || x_diff > -2) && (y_diff < 2 || y_diff > -2) {
+                    let x_diff = prev_knot_pos.0 - curr_knot_pos.0;
+                    let y_diff = prev_knot_pos.1 - curr_knot_pos.1;
 
-                } else {
-                    panic!("Unexpected position difference: ({x_diff}, {y_diff})")
+                    if x_diff == 2 && y_diff == 0 {
+                        curr_knot_pos.0 += 1;
+                    } else if x_diff == -2 && y_diff == 0 {
+                        curr_knot_pos.0 -= 1;
+                    } else if y_diff == 2 && x_diff == 0 {
+                        curr_knot_pos.1 += 1;
+                    } else if y_diff == -2 && x_diff == 0 {
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == 2 && y_diff == 1 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 += 1;
+                    } else if x_diff == -2 && y_diff == 1 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 += 1;
+                    } else if x_diff == 2 && y_diff == -1 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == -2 && y_diff == -1 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == 1 && y_diff == 2 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 += 1;
+                    } else if x_diff == 1 && y_diff == -2 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == -1 && y_diff == 2 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 += 1;
+                    } else if x_diff == -1 && y_diff == -2 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == 2 && y_diff == 2 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 += 1;
+                    } else if x_diff == 2 && y_diff == -2 {
+                        curr_knot_pos.0 += 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == -2 && y_diff == -2 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 -= 1;
+                    } else if x_diff == -2 && y_diff == 2 {
+                        curr_knot_pos.0 -= 1;
+                        curr_knot_pos.1 += 1;
+                    } else if (x_diff < 2 && x_diff > -2) && (y_diff < 2 && y_diff > -2) {
+
+                    } else {
+                        panic!("Unexpected position difference: ({x_diff}, {y_diff})")
+                    }
+
+                    knots_pos[i] = curr_knot_pos;
+                    prev_knot_pos = curr_knot_pos;
                 }
 
-                println!("{:?}",h_pos);
-                println!("{:?}",t_pos);
+                t_pos_set.insert(knots_pos[8]);
+            }
 
-                t_pos_set.insert(t_pos);
+            println!("{:?}",h_pos);
+            for k_pos in knots_pos {
+                println!("{:?}",k_pos);
             }
         }
 
