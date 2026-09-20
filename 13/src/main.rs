68cc6de183a8ba8f5173a2a3c71248602661a218
AR <repattila@protonmail.com>
2026-09-20 21:54:55 +0200

    parsing lines as json

--- 13/src/main.rs
+++ 13/src/main.rs
@@ -1,3 +1,32 @@
+use aoc22_shared::*;
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(lines) = read_lines("res/example1.txt") {
+        let mut right: Option<Value> = None;
+        let mut left: Option<Value> = None;
+        for line in lines.map_while(Result::ok) {
+            if !line.is_empty() {
+                println!("{line}");
+
+                let val: Value = match serde_json::from_str(&line) {
+                        Ok(res) => res,
+                        _ => std::process::exit(1)
+                    };
+                if left.is_none() {
+                    println!("val = {:?}", val);
+                    left = Some(val);
+                } else {
+                    println!("val = {:?}", val);
+                    right = Some(val);
+                }
+            } else {
+                println!("left = {:?}", left);
+                println!("right = {:?}", right);
+
+                right = None;
+                left = None;
+            }
+        }
+    }
 }
